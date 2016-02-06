5c5515d9299ddc12c1853ec1199011668a7d149c
Adam Weitzman <adamcweitzman@gmail.com>
2016-02-06 15:40:58 -0600

    getting closer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,8 +8,20 @@ app.config(['$routeProvider', function($routeProvider) {
       controller: 'MainController'
     })
 
-}])
+}]);
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	console.log('in scope')
+
+	$scope.processForm = function() {
+		console.log('in form process')
+		$http.get('http://api.indeed.com/ads/apisearch?publisher=2878037053725137&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=FJR&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2')
+ 		.success(function(data) {
+ 			console.log(data);
+        })
+	};
 }]);
+
+// publisher=2878037053725137
+
+// chnl=FJR
